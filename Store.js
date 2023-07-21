3dd4157b9c316b25a59374c1afd4d80d76ee1f8c
kelvin obiri <obirikan020@gmail.com>
2023-07-21 16:50:49 -0700

    Update Store.js

--- Store.js
+++ Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import navReducer from './slices/navSlice'
-
+//didnt use the store i sed context
 export const store = configureStore({
   reducer:{
     nav:navReducer
